0e52d1644594b0486ad7a25bccf71b247005c7b4
Pedro P. Alvarez Portelles <ppalvarez2002@gmail.com>
2024-11-22 16:58:09 -0500

    Improve server streaming

--- backend/dispotify/serializers.py
+++ backend/dispotify/serializers.py
@@ -1,6 +1,5 @@
-import os, io
+import io
 import uuid
-import time
 from pydub import AudioSegment
 import struct, base64
 from .models import *
@@ -8,7 +7,6 @@ from dataclasses import dataclass
 from rest_framework import serializers
 from django.core.files.base import ContentFile
 
-HEADER_SIZE = 44
 CHUNK_SIZE = 1 << 15 #32kB
 
 @dataclass
@@ -32,41 +30,36 @@ class AudioStreamerSerializer(serializers.Serializer):
         chunk_index : int = data['chunk_index']
         chunk_count : int = data['chunk_count']
         audio_id : str = data['audio_id']
-        include_header : bool = data['include_header']
         include_metadata : bool = data['include_metadata']
 
         filename = self.get_file_name(audio_id)
     
     
         with open(filename, 'rb') as wav_file:
-            header = wav_file.read(HEADER_SIZE)
-
             wav_file.seek(0, 2)
             file_size = wav_file.tell()
-            audio_data_size = file_size - HEADER_SIZE
+            audio_data_size = file_size
             total_chunks = (audio_data_size + CHUNK_SIZE - 1) // CHUNK_SIZE
             
             response = {
                 'chunk_index': chunk_index,
                 'chunk_count': min(chunk_count, total_chunks - chunk_index  ),
             }
-
-            if include_header:
-                response['header'] = base64.b64encode(header)
             
             if include_metadata:
-                channels = struct.unpack_from('<H', header, 22)[0]
-                sample_rate = struct.unpack_from('<I', header, 24)[0]
-                bits_per_sample = struct.unpack_from('<H', header, 34)[0]
-                duration = audio_data_size / (bits_per_sample // 8) / sample_rate / channels
+                song = Song.objects.get(id=audio_id)
+                
+                channels = 2 # TODO
+                bitrate = song.bitrate
+                duration = song.duration_seconds
 
                 response['metadata'] = {
                     'channels' : channels,
-                    'sample_rate' : sample_rate,
-                    'bits_per_sample' : bits_per_sample,
                     'duration' : duration,
                     'total_chunks': total_chunks,
-                    'chunk_size': CHUNK_SIZE
+                    'chunk_size': CHUNK_SIZE,
+                    'bitrate' : bitrate,
+                    'file_size': file_size,
                 }
             
             chunks = []
@@ -75,10 +68,10 @@ class AudioStreamerSerializer(serializers.Serializer):
                 if current_chunk_index >= total_chunks:
                     break
                 
-                offset = HEADER_SIZE + (current_chunk_index * CHUNK_SIZE)
+                offset = current_chunk_index * CHUNK_SIZE
                 wav_file.seek(offset)
                 
-                chunk_data = wav_file.read(CHUNK_SIZE)
+                chunk_data = wav_file.read(min(CHUNK_SIZE, file_size - offset))
                 chunk_data = base64.b64encode(chunk_data)
 
                 chunks.append(chunk_data)
@@ -89,7 +82,7 @@ class AudioStreamerSerializer(serializers.Serializer):
 
 
     def get_file_name(self, audio_id: str):
-        return f'../audios/{audio_id}.wav'
+        return f'../audios/{audio_id}'
 
 
 class ArtistSerializer(serializers.ModelSerializer):
